d8ea0c82bb673271fd74196164c462b10e9d78d8
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2024-11-22 23:26:46 +0100

    add use server on actions file

--- src/app/election/page.tsx
+++ src/app/election/page.tsx
@@ -2,9 +2,12 @@ import { createElectionAction } from "@/features/election/actions";
 
 export default function Election() {
   return (
-    <form action={createElectionAction}>
-      <input type="text" name="electionName"/>
-      <button type="submit">Create Election</button>
-    </form>
+    <>
+    <h1>Create new Election</h1>
+      <form action={createElectionAction}>
+        <input type="text" name="electionName" />
+        <button type="submit">Create Election</button>
+      </form>
+    </>
   );
 }
