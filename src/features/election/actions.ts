0b493a680f4c329a8bad0aa973010dd1677b5b1a
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2024-11-23 00:04:55 +0100

    call create election on action

--- src/features/election/actions.ts
+++ src/features/election/actions.ts
@@ -1,4 +1,8 @@
 "use server"
+
+import { electionFeature } from "./instance";
+
 export async function createElectionAction(formData: FormData) {
   const electionName = formData.get("electionName");
+  await electionFeature.service.createElection(electionName)
 }
