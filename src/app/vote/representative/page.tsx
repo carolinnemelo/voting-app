5e8d5eb4fc6803a71342c69bef0319527f3a537a
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2024-11-23 23:19:19 +0100

    delete content representative route

--- src/app/vote/representative/page.tsx
+++ src/app/vote/representative/page.tsx
@@ -1,15 +1,7 @@
-import { issueFeature } from "@/features";
-import { SelectIssue } from "@/features/issue/components/select-issue";
-import { SelectChoices } from "@/features/vote/components/select-choices";
-
 export default async function Representative() {
-  const choicesTableRows = await issueFeature.service.getAllChoices();
-
   return (
     <>
       <h1>Representative Vote Page</h1>
-      <SelectIssue />
-      <SelectChoices choicesTableRows={choicesTableRows}/>
     </>
   );
 }
