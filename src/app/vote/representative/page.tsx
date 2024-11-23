a5838ccb25dde921c13a35de7059ca5854f96bb5
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2024-11-23 17:17:20 +0100

    create function that generates choice list

--- src/app/vote/representative/page.tsx
+++ src/app/vote/representative/page.tsx
@@ -1,4 +1,5 @@
-import { issueFeature } from "@/features";
+import { choicesTable } from "@/db";
+import { issueFeature, voteOnIssueAction } from "@/features";
 
 export default async function Representative() {
   const issueList = await generateIssueList();
@@ -12,6 +13,7 @@ export default async function Representative() {
           {issueList}
         </select>
       </form>
+      <form action=""></form>
     </>
   );
 }
@@ -26,4 +28,16 @@ async function generateIssueList() {
     );
   });
   return issueList;
+}
+
+async function generateChoiceList() {
+  const choicesTableRows = await issueFeature.service.getAllChoices();
+  const choiceList = choicesTableRows.map((choice) => {
+    return (
+      <option key={choice.id} value={choice.id}>
+        {choice.}
+      </option>
+    );
+  });
+  return choiceList;
 }
