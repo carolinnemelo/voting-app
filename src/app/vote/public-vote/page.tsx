3c174c50c022c120aa9bdb7bd7f2da390ebdcb3e
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2024-11-23 12:59:54 +0100

    add public vote form

---
+++ src/app/vote/public-vote/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return( 
+  <>
+  <h1>Public Vote Form</h1>
+  <form action="">
+    <label htmlFor="publicVoteName">Public Vote Name</label>
+    <input type="text" name="publicVoteName" id="publicVoteName" />
+  </form>
+  </>
+
+  );
+}
