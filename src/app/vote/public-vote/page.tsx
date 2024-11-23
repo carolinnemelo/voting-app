c39155c0a7ea352c28663843da931ae45edd60b2
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2024-11-23 13:06:27 +0100

    add select tag and call election feature to get list of representatives

--- src/app/vote/public-vote/page.tsx
+++ src/app/vote/public-vote/page.tsx
@@ -1,12 +1,18 @@
-export default function Home() {
-  return( 
-  <>
-  <h1>Public Vote Form</h1>
-  <form action="">
-    <label htmlFor="publicVoteName">Public Vote Name</label>
-    <input type="text" name="publicVoteName" id="publicVoteName" />
-  </form>
-  </>
-
+export default async function Home() {
+  const representativeList =
+  await electionFeature.service.getAllRepresentatives();
+  return (
+    <>
+      <h1>Public Vote Form</h1>
+      <form action="">
+        <label htmlFor="publicVoteName">Public Vote Name</label>
+        <input type="text" name="publicVoteName" id="publicVoteName" />
+        <label htmlFor="representativeSelect">Choose Representative</label>
+        <select name="representativeSelect" id="representativeSelect">
+          <option value="">Please choose an option</option>
+          {representativeList}
+        </select>
+      </form>
+    </>
   );
 }
