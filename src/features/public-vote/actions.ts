7ca806b7a770dede7e33e4c04a0d9e06a5aa0d16
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2024-11-23 13:37:56 +0100

    call service to create and vote on public vote feature

--- src/features/public-vote/actions.ts
+++ src/features/public-vote/actions.ts
@@ -3,7 +3,8 @@ export async function createPublicVoteAndVoteAction(formData: FormData) {
   if(!formData) {
     return;
   }
-  // const publicVoteName = formData.get("publicVoteName") as string;
-  console.log(formData)
+  const publicVoteName = formData.get("publicVoteName") as string;
+  const representiveId = formData.get("representativeSelect") as string;
+  await publicVoteFeature.service.createPublicVoteAndVote();
 
 }
