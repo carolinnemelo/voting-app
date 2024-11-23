67ee325b052d151ba5fdd07a4c651fa42b074495
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2024-11-23 17:07:44 +0100

    create select with a function that will generate the option

--- src/app/vote/representative/page.tsx
+++ src/app/vote/representative/page.tsx
@@ -1,17 +1,29 @@
-import { issueFeature } from "@/features"
+import { issueFeature } from "@/features";
 
 export default async function Representative() {
-  const issueList = issueFeature.service.getAll()
+  const issueList = await generateIssueList();
+
   return (
-  <>
-  <h1>Representative Vote Page</h1>
-  <form action="">
-    <select name="issueSelect" id="issueSelect">
-    <option value="">Please choose an option</option>
+    <>
+      <h1>Representative Vote Page</h1>
+      <form action="">
+        <select name="issueSelect" id="issueSelect">
+          <option value="">Please choose an option</option>
+          {issueList}
+        </select>
+      </form>
+    </>
+  );
+}
 
-    </select>
-  </form>
-  </>
-
-  )
+async function generateIssueList() {
+  const issuesTableRows = await issueFeature.service.getAll();
+  const issueList = issuesTableRows.map((issue) => {
+    return (
+      <option key={issue.id} value={issue.id}>
+        {issue.issueName}
+      </option>
+    );
+  });
+  return issueList;
 }
