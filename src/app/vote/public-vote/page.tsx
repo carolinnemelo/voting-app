5fc3c6a8b47e7c0caa477341b75378f4c9c1647c
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2024-11-23 13:19:14 +0100

    create function that generates the representative options to display on the select

--- src/app/vote/public-vote/page.tsx
+++ src/app/vote/public-vote/page.tsx
@@ -1,6 +1,7 @@
+import { electionFeature } from "@/features/election/instance";
+
 export default async function Home() {
-  const representativeList =
-  await electionFeature.service.getAllRepresentatives();
+  const representativeList = await generateRepresentativeList()
   return (
     <>
       <h1>Public Vote Form</h1>
@@ -15,4 +16,12 @@ export default async function Home() {
       </form>
     </>
   );
+}
+
+async function generateRepresentativeList() {
+  const representativesTableRows = await electionFeature.service.getAllRepresentatives();
+  const representativeList  = representativesTableRows.map((representative) => {
+    return <option key={representative.id} value={representative.id}>{representative.representativeName}</option>
+  })
+  return representativeList;
 }
