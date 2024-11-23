c4e7bdf787b9185c99001dd277a3650d8fbfffad
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2024-11-23 11:18:55 +0100

    create form to representative creation

--- src/app/election/new-representative/page.tsx
+++ src/app/election/new-representative/page.tsx
@@ -1,3 +1,12 @@
 export default function NewRepresentative() {
-  return <h1>Add new representative Page</h1>;
+  return (
+    <>
+      <h1>Add new representative Page</h1>
+      <form>
+        <input type="text" name="representativeName" />
+        <input type="email" name="email" />
+        <button type="submit">Create Election</button>
+      </form>
+    </>
+  );
 }
