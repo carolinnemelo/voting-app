9e6aa76de938bd3e3db730790a730012d1ba3d77
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2024-11-23 23:26:15 +0100

    map mocked array

--- src/app/vote/representative/page.tsx
+++ src/app/vote/representative/page.tsx
@@ -1,7 +1,14 @@
+const issueList = ["Animals", "Colors", "Best season"];
+
 export default async function Representative() {
   return (
     <>
       <h1>Representative Vote Page</h1>
+      <ul>
+        {issueList.map((issue, index) => (
+          <li key={index}>{issue}</li>
+        ))}
+      </ul>
     </>
   );
 }
