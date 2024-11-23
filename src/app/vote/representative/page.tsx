fbf44ae11eefb14ca7ea9e9a1add9ea5d17959bd
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2024-11-23 23:29:12 +0100

    mock object

--- src/app/vote/representative/page.tsx
+++ src/app/vote/representative/page.tsx
@@ -1,12 +1,39 @@
-const issueList = ["Animals", "Colors", "Best season"];
+
+const mockData = [
+  {
+    id: 1,
+    issueName: "Cats vs Dogs",
+    choices: [
+      { id: 101, choiceName: "Cats" },
+      { id: 102, choiceName: "Dogs" },
+    ],
+  },
+  {
+    id: 2,
+    issueName: "Pizza vs Burger",
+    choices: [
+      { id: 201, choiceName: "Pizza" },
+      { id: 202, choiceName: "Burger" },
+    ],
+  },
+  {
+    id: 3,
+    issueName: "Tea vs Coffee",
+    choices: [
+      { id: 301, choiceName: "Tea" },
+      { id: 302, choiceName: "Coffee" },
+    ],
+  },
+];
+
 
 export default async function Representative() {
   return (
     <>
       <h1>Representative Vote Page</h1>
       <ul>
-        {issueList.map((issue, index) => (
-          <li key={index}>{issue}</li>
+        {mockData.map((issue) => (
+          <li key={issue.id}>{issue.issueName}</li>
         ))}
       </ul>
     </>
