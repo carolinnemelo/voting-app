c0070e561ef22d176b269e36693f1d9bcd5a3a10
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2024-11-23 11:19:31 +0100

    call server action on representative form

--- src/app/election/new-representative/page.tsx
+++ src/app/election/new-representative/page.tsx
@@ -2,7 +2,7 @@ export default function NewRepresentative() {
   return (
     <>
       <h1>Add new representative Page</h1>
-      <form>
+      <form action={createRepresentativeAction}>
         <input type="text" name="representativeName" />
         <input type="email" name="email" />
         <button type="submit">Create Election</button>
