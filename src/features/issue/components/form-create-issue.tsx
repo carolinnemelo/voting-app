f9c454d09ab2d05862219d696193e258923dd02a
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2024-11-24 20:33:08 +0100

    create component to minimize code on form

--- src/features/issue/components/form-create-issue.tsx
+++ src/features/issue/components/form-create-issue.tsx
@@ -10,35 +10,30 @@ export function Form() {
   return (
     <form action={formAction}>
       <Input typeOfInput="text" label="Issue name" nameOfInput="issueName" />
-      {state?.errors.issueName &&
-        state.errors.issueName.map((error) => {
-          return (
-            <p className="mt-2 text-sm text-red-500" key={error}>
-              {error}
-            </p>
-          );
-        })}
+      {state?.errors.issueName && <ErrorMessage errors={state.errors.issueName} />}
+    
       <Input typeOfInput="text" label="Choice 1" nameOfInput="choice1" />
-      {state?.errors.choice1 &&
-        state.errors.choice1.map((error) => {
-          return (
-            <p className="mt-2 text-sm text-red-500" key={error}>
-              {error}
-            </p>
-          );
-        })}
+      {state?.errors.choice1 && <ErrorMessage errors={state.errors.choice1} />}
+
       <Input typeOfInput="text" label="Choice 2" nameOfInput="choice2" />
-      {state?.errors.choice2 &&
-        state.errors.choice2.map((error) => {
-          return (
-            <p className="mt-2 text-sm text-red-500" key={error}>
-              {error}
-            </p>
-          );
-        })}
+      {state?.errors.choice2 && <ErrorMessage errors={state.errors.choice2} />}
 
       <Button label="Create issue" type="submit" />
       <div id="customer-error" aria-live="polite" aria-atomic="true"></div>
     </form>
   );
+}
+
+type Props = {
+  errors: string[];
+};
+
+export function ErrorMessage({ errors }: Props) {
+  return (errors.map((error) => {
+    return (
+      <p className="mt-2 text-sm text-red-500" key={error}>
+        {error}
+      </p>
+    );
+  }))
 }
