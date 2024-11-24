799d32f9cecddf6cba91cf311ee4e912f036dace
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2024-11-24 22:54:33 +0100

    render error message from zod for create representative

--- src/features/issue/components/form-new-representative.tsx
+++ src/features/issue/components/form-new-representative.tsx
@@ -3,6 +3,7 @@
 import { useActionState } from "react";
 import { createRepresentativeAction } from "../actions";
 import { Button, Input } from "@/components";
+import { ErrorMessage } from "./error-message";
 
 export function FormNewRepresentative() {
   const initialState = { message: null, errors: {} };
@@ -11,7 +12,13 @@ export function FormNewRepresentative() {
   return (
     <form action={formAction}>
       <Input typeOfInput="text" label="Representative name" nameOfInput="representativeName" />
+      {state?.errors.representativeName && (
+        <ErrorMessage errors={state.errors.representativeName} />
+      )}
       <Input typeOfInput="email" label="Email" nameOfInput="email" />
+      {state?.errors.email && (
+        <ErrorMessage errors={state.errors.email} />
+      )}
       <Button label="Create Representative" type="submit" />
     </form>
   );
