e2473313344637b8ce7344f34f9c092554dab273
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2024-11-24 18:35:33 +0100

    add accordion

--- src/components/reusable-accordion.tsx
+++ src/components/reusable-accordion.tsx
@@ -7,16 +7,19 @@ import {
 
 type Props = {
   label: string;
-  list: []
-}
+  list: string[];
+};
 
-export function ReusableAccordion({label, list}: Props) {
- return ( <Accordion type="single" collapsible>
-    <AccordionItem value="item-1">
-      <AccordionTrigger>{label}</AccordionTrigger>
-      <AccordionContent>
-        {}
-      </AccordionContent>
-    </AccordionItem>
-  </Accordion>)
+export function ReusableAccordion({ label, list }: Props) {
+  const content = list.map((e, index) => {
+    return <AccordionContent key={index}>{e}</AccordionContent>;
+  });
+  return (
+    <Accordion className="my-3" type="single" collapsible>
+      <AccordionItem value="item-1">
+        <AccordionTrigger>{label}</AccordionTrigger>
+        {content}
+      </AccordionItem>
+    </Accordion>
+  );
 }
