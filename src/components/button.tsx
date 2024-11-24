5133dada330bffd51c60bf3fb33571394d326efa
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2024-11-24 15:59:39 +0100

    use shad style

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,8 +1,10 @@
+import { ButtonShad } from "./shad-components/button-shad";
+
 type Props = {
   label: string;
   type: "button" | "submit" | "reset";
 };
 
 export function Button({ label, type }: Props) {
-  return <button type={type}>{label}</button>;
+  return <ButtonShad type={type}>{label}</ButtonShad>;
 }
