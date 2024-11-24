d19b8e9af9987702aa0011499651b893bc03a90e
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2024-11-24 17:57:13 +0100

    change card name

--- src/app/issue/page.tsx
+++ src/app/issue/page.tsx
@@ -1,4 +1,5 @@
-import { ReusableCard } from "@/components";
+import { Button, Input, ReusableCard } from "@/components";
+import { Main } from "@/components/main";
 import { issueFeature, createIssueAction } from "@/features";
 
 const issueList = await issueFeature.service.getAllIssuesAndChoices();
@@ -6,18 +7,18 @@ const issueList = await issueFeature.service.getAllIssuesAndChoices();
 export default function Issue() {
   return (
     <>
-      <ReusableCard>
-        <h1>Create new Issue</h1>
-        <pre>{JSON.stringify(issueList, null, 2)}</pre>
-        <form action={createIssueAction}>
-          <input type="text" name="issueName" />
-          <label htmlFor="choice1">Choice 1</label>
-          <input type="text" name="choice1" id="choice1" />
-          <label htmlFor="choice2">Choice 2</label>
-          <input type="text" name="choice2" id="choice2" />
-          <button type="submit">Create Issue</button>
-        </form>
-      </ReusableCard>
+      <Main>
+        <ReusableCard>
+          <h1>Create new Issue</h1>
+          <pre>{JSON.stringify(issueList, null, 2)}</pre>
+          <form action={createIssueAction}>
+            <Input typeOfInput="text" label="Issue name" nameOfInput="issueName" />
+            <Input typeOfInput="text" label="Choice 1" nameOfInput="choice1" />
+            <Input typeOfInput="text" label="Choice 2" nameOfInput="choice2" />
+            <Button label="Create issue" type="submit"/>
+          </form>
+        </ReusableCard>
+      </Main>
     </>
   );
 }
