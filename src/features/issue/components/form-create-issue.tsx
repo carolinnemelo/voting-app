18291b221405e697ea5995cfa40acd044f26eb88
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2024-12-01 21:05:07 +0100

    remove use state

--- src/features/issue/components/form-create-issue.tsx
+++ src/features/issue/components/form-create-issue.tsx
@@ -1,25 +1,14 @@
 "use client";
 import { Button, Input} from "@/components";
-import { createIssueAction, State } from "../actions";
-import { useActionState } from "react";
-import { ErrorMessage } from ".";
+import { createIssueAction } from "../actions";
 
 export function FormCreateIssue() {
-  const initialState: State = { message: null, errors: {} };
-  const [state, formAction] = useActionState(createIssueAction, initialState);
-
   return (
-    <form action={formAction}>
+    <form action={createIssueAction}>
       <Input typeOfInput="text" label="Issue name" nameOfInput="issueName" />
-      {state?.errors.issueName && (
-        <ErrorMessage errors={state.errors.issueName} />
-      )}
-
       <Input typeOfInput="text" label="Choice 1" nameOfInput="choice1" />
-      {state?.errors.choice1 && <ErrorMessage errors={state.errors.choice1} />}
 
       <Input typeOfInput="text" label="Choice 2" nameOfInput="choice2" />
-      {state?.errors.choice2 && <ErrorMessage errors={state.errors.choice2} />}
 
       <Button label="Create issue" type="submit" />
     </form>
