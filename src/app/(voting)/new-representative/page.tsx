228d059a3e52537561f90d03567d45c72c20b92c
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2024-12-07 21:50:00 +0100

    remove coments

--- src/app/(voting)/new-representative/page.tsx
+++ src/app/(voting)/new-representative/page.tsx
@@ -1,23 +1,14 @@
-import {  ReusableCard } from "@/components";
+import { ReusableCard } from "@/components";
 import { Main } from "@/components/main";
-// import {  issueFeature } from "@/features";
-import { FormNewRepresentative } from "@/features/issue/components/form-new-representative";
+
+import { FormNewRepresentative } from "@/features";
 
 export default async function NewRepresentative() {
-  // const representativeList = await issueFeature.service.getAllRepresentatives();
-  // const representativeNamesList = representativeList.map(
-  //   (representative) => representative.representativeName
-  // );
   return (
     <>
       <Main>
         <ReusableCard cardTitle="Add new representative Page">
-          AI MEU DEUS
-          {/* <ReusableAccordion
-            label="All Representatives"
-            list={representativeNamesList}
-          />  */}
-           <FormNewRepresentative />
+         <FormNewRepresentative />
         </ReusableCard>
       </Main>
     </>
