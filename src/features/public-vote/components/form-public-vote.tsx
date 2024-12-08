1078ba9d69d1d1592faf787f6987f9a361e94d3a
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2024-12-08 09:13:13 +0100

    use form from next.js and call for action

--- src/features/public-vote/components/form-public-vote.tsx
+++ src/features/public-vote/components/form-public-vote.tsx
@@ -1,14 +1,16 @@
 import { Select } from "@/features/public-vote/components/select";
 import { Representative } from "./types";
 import { Button } from "@/components";
+import Form from 'next/form'
+
 
 type Props = { representatives: Representative[] };
 
 export function FormPublicVote({ representatives }: Props) {
   return (
-    <>
+    <Form action={voteAction}>
       <Select representatives={representatives} />
       <Button type="submit" label="Vote"/>
-    </>
+    </Form>
   );
 }
