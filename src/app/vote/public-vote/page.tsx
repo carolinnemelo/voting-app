5b553dfbefbf8e8f76e1da94db61e76bed9b4b38
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2024-12-08 08:58:11 +0100

    rename variable

--- src/app/vote/public-vote/page.tsx
+++ src/app/vote/public-vote/page.tsx
@@ -1,15 +1,15 @@
 import { ReusableCard } from "@/components";
 import { Main } from "@/components/main";
-import { issueService } from "@/features";
+import { issueService, FormPublicVote } from "@/features";
 
 export default async function Home() {
-  const representativesTableRows =
+  const representatives =
     await issueService.getAllRepresentatives();
   return (
     <>
       <Main>
         <ReusableCard cardTitle="Public Vote Form">
-          <FormPublicVote representativeTableRows={representativesTableRows} />
+          <FormPublicVote representatives={representatives} />
         </ReusableCard>
       </Main>
     </>
