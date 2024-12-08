ba637b2d8247f0e4d3f00273cc8feb790e151d2e
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2024-12-08 08:58:47 +0100

    rename variable

--- src/features/public-vote/components/form-public-vote.tsx
+++ src/features/public-vote/components/form-public-vote.tsx
@@ -1,13 +1,14 @@
 import { Select } from "@/features/public-vote/components/select";
-import { RepresentativesTableRows } from "./types";
+import { Representative } from "./types";
+import { Button } from "@/components";
 
-type Props = { representatives: RepresentativesTableRows[] };
+type Props = { representatives: Representative[] };
 
 export function FormPublicVote({ representatives }: Props) {
   return (
     <>
-    
-      <Select representativeTableRows={representatives} />
+      <Select representatives={representatives} />
+      <Button type="submit" label="Vote"/>
     </>
   );
 }
