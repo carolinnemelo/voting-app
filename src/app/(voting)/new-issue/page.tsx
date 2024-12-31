b25953b7519a1b5c93a36d0e98748f715191d567
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2024-12-31 13:06:16 +0100

    imports in IssuePage component

--- src/app/(voting)/new-issue/page.tsx
+++ src/app/(voting)/new-issue/page.tsx
@@ -1,12 +1,11 @@
-import { ReusableCard } from "@/components";
-import { Main } from "@/components/main";
+import { ReusableCard, Main } from "@/components";
 import { FormCreateIssue } from "@/features";
 
 export default function IssuePage() {
   return (
     <Main>
       <ReusableCard cardTitle="Create Issue">
-      <FormCreateIssue />
+        <FormCreateIssue />
       </ReusableCard>
     </Main>
   );
