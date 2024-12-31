6fbc5926a5b31eacc831ecc385fe0a258f5f8f97
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2024-12-31 13:07:30 +0100

    replace NavigationMenu with DesktopNav and MobileNav components

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,49 +1,11 @@
-import Link from "next/link";
-import {
-  NavigationMenu,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  navigationMenuTriggerStyle,
-} from "./shad-components";
+import { DesktopNav } from "./desktop-nav";
+import { MobileNav } from "./mobile-nav";
 
 export function Navbar() {
   return (
-    <NavigationMenu className="top-0">
-      <NavigationMenuList>
-        <NavigationMenuItem>
-          <Link href="/new-issue" legacyBehavior passHref>
-            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-              New Issue
-            </NavigationMenuLink>
-          </Link>
-        </NavigationMenuItem>
-
-        <NavigationMenuItem>
-          <Link href="/new-representative" legacyBehavior passHref>
-            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-              New Representative
-            </NavigationMenuLink>
-          </Link>
-        </NavigationMenuItem>
-
-
-        <NavigationMenuItem>
-          <Link href="/vote/public-vote" legacyBehavior passHref>
-            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-              Public Vote
-            </NavigationMenuLink>
-          </Link>
-        </NavigationMenuItem>
-        
-        <NavigationMenuItem>
-          <Link href="/vote/statistics" legacyBehavior passHref>
-            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-              Statistics
-            </NavigationMenuLink>
-          </Link>
-        </NavigationMenuItem>
-      </NavigationMenuList>
-    </NavigationMenu>
+    <>
+      <DesktopNav />
+      <MobileNav />
+    </>
   );
 }
