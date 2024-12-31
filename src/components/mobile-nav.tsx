b63f6332e1704751588bcf895e1e214726349dbd
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2024-12-31 13:20:35 +0100

    remove debug console log from MobileNav component

--- src/components/mobile-nav.tsx
+++ src/components/mobile-nav.tsx
@@ -11,7 +11,6 @@ export function MobileNav() {
         className="md:hidden m-2 p-1 z-20 absolute top-0"
         onClick={() => {
           setIsOpen(!isOpen);
-          console.log({ isOpen });
         }}
       >
         {isOpen ? <XIcon size={30} /> : <Menu size={30} />}
