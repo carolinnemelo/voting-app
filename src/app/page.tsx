c0f2f3264af5fbea4ae2a06b4d5ffd2721023def
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2024-12-31 13:19:16 +0100

    refactor Home component to use Card instead of ReusableCard for improved layout and styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,26 @@
-import { ReusableCard, Main } from "@/components";
+import { Main, Card, CardTitle, CardContent } from "@/components";
 
 export default function Home() {
   return (
     <Main>
-      <ReusableCard cardTitle="Welcome to Voting app">
-      <p>
-        Effortless voting and representative management. Track Public Votes:
-        Stay updated on voting results with clear and simple insights. Manage
-        Representatives: Add, edit, and review representatives with ease. Get
-        Instant Statistics: See voting data and trends at a glance. Everything
-        you need in one place. Quick, easy, and reliable.
-      </p>
-      </ReusableCard>
-  
+      <Card className="my-auto p-4 flex-grow max-w-full flex-wrap text-center">
+        <CardTitle className="mb-3 text-5xl">Welcome to Voting app</CardTitle>
+        <CardContent>
+          <p className="text-2xl font-medium py-2">
+            Effortless voting and representative management.
+          </p>
+          <p>
+            Manage Representatives: Add, edit, and review representatives with
+            ease.
+          </p>
+          <p>
+            Track Public Votes: Stay updated on voting results with clear and
+            simple insights.
+          </p>
+          <p>Get Instant Statistics: See voting data and trends at a glance.</p>
+          <p>Everything you need in one place. Quick, easy, and reliable.</p>
+        </CardContent>
+      </Card>
     </Main>
   );
 }
